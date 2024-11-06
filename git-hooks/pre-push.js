f8b1b093b7212bb5adc73774d4b07566cecd1104
David Berg <david.nossebro@gmail.com>
2024-11-06 21:02:53 +0100

    Added pre-push.js to manage pre-push hook.

---
+++ git-hooks/pre-push.js
@@ -0,0 +1,8 @@
+import { execSync } from 'child_process';
+
+const branch = execSync('git rev-parse --abbrev-ref HEAD')
+    .toString().trim();
+
+if (branch === 'main') {
+    execSync('npm run deploy', { stdio: 'inherit' })
+}
