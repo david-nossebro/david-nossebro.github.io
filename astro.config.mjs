e0a8a3e9014ded9ddb269caf7bb66dc4a2fb991c
David Berg <david.nossebro@gmail.com>
2024-11-09 00:12:49 +0100

    Rollback of changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://david-nossebro.github.io',
-	base: '/',
 	integrations: [mdx(), sitemap()],
 });
