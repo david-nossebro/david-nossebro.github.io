e74aee8073eba04f46d6c0dd553850cc521f274a
David Berg <david.nossebro@gmail.com>
2024-11-14 17:58:47 +0100

    Removed tailwind typography and added my own base-style instead.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,31 +4,7 @@ import theme from "tailwindcss/defaultTheme.js";
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			typography: (theme) => ({
-				DEFAULT: {
-					css: {
-						// Remove pre and post '"' of blockquotes
-						'blockquote p:first-of-type::before': false,
-						'blockquote p:first-of-type::after': false,
-						'blockquote': {
-							'font-style': 'normal'
-						},
-
-						// Style ´code´ with gray background without single ` around
-						'code::before': false,
-						'code::after': false,
-						'code': {
-							'background-color': theme('colors.gray.300'),
-							'padding-left': '4px',
-							'padding-right': '4px'
-						}
-					},
-				},
-			}),
-		},
+		extend: {},
 	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
+	plugins: [],
 }
